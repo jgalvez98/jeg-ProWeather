7bfd32b1afd9280feb60df7cd493206e7c46c51e
juanes G <jgalvez98@gmail.com>
2023-07-27 15:07:27 -0400

    js variables added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var apiKey="26b8767a0c9a67d5cba18ad715da7418"
+var titleEl=document.getElementById("title")
+var tempEl=document.getElementById("temp")
+var windEl=document.getElementById("wind")
+var humidityEl=document.getElementById("humidty")
+var searchBtn=document.getElementById("search-btn")
+var cityInputEl=document.getElementById("city-input")
+
+
